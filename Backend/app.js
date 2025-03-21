524970d4fe867f3766360eea1ec192f5b5023ab1
Mohd Arif <ma9918127@gmail.com>
2025-03-21 23:37:34 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -13,13 +13,7 @@ app.use(
     })
   );
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.get("/",(req,res)=>{
- res.json({
-  success:true,
-  message:"Done",
-});
-});
+
 app.use("/api/v1/message", messageRouter);
 dbConnection();
 
